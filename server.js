44515a933b117fd8582b08edbf4fc0cc290bf19a
Eczbek <eczbek.void@gmail.com>
2022-04-28 09:01:00 -0400

    Fix typo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ createServer((request, response) => {
 	if (request.url === '/subscribe')
 		return subscribers.push(response);
 	if (request.url === '/publish' && request.method === 'POST') {
-		subscribers.forEach((subscriber) => request.pipe(subscriber);
+		subscribers.forEach((subscriber) => request.pipe(subscriber));
 		subscribers.length = 0;
 	}
 	createReadStream('./client.html').pipe(response);
